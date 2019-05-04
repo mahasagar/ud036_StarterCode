6aa6d10c800fd2aec312ef4fcad760a6559c7d96
Sagar Chaudhari <sagar.chaudhari@medibox.in>
2019-05-04 08:01:03 +0530

    Bugfixes and Comments added.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,31 +1,36 @@
 import media
 import fresh_tomatoes
-avengers = media.Movie('Avengers: Endgame',
-"https://image.tmdb.org/t/p/w600_and_h900_bestv2/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
-"https://www.youtube.com/watch?v=TcMBFSGVi1c");
 
-#print(avengers.storyline);
-#avengers.show_trailer();
+# build instances of several movies with title, description,
+# poster url and trailer youtube url.
+avengers = media.Movie('Avengers: Endgame',
+                       "https://image.tmdb.org/t/p/w600_and_h900_bestv" +
+                       "2/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
+                       "https://www.youtube.com/watch?v=TcMBFSGVi1c")
 
 mad_max = media.Movie('Mad Max',
-"https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg",
-"https://youtu.be/akX3Is3qBpw");
+                      "https://image.tmdb.org/t/p/w500/kqjL17yufvn9OV" +
+                      "LyXYpvtyrFfak.jpg",
+                      "https://youtu.be/akX3Is3qBpw")
 
 
 avatar = media.Movie('Avatar',
-"https://image.tmdb.org/t/p/w600_and_h900_bestv2/kmcqlZGaSh20zpTbuoF0Cdn07dT.jpg",
-"https://youtu.be/5MB3Ea6L-gw");
+                     "https://image.tmdb.org/t/p/w600_and_h900_bestv2/kmc" +
+                     "qlZGaSh20zpTbuoF0Cdn07dT.jpg",
+                     "https://youtu.be/5MB3Ea6L-gw")
 
 
 zombieland = media.Movie('Zombieland',
-"https://image.tmdb.org/t/p/w600_and_h900_bestv2/vUzzDpVrab1BOG3ogxhRGfLN94d.jpg",
-"https://youtu.be/8m9EVP8X7N8");
+                         "https://image.tmdb.org/t/p/w600_and_h900_bes" +
+                         "tv2/vUzzDpVrab1BOG3ogxhRGfLN94d.jpg",
+                         "https://youtu.be/8m9EVP8X7N8")
 
 
 infinity_war = media.Movie('Avengers: Infinity War',
-"https://image.tmdb.org/t/p/w600_and_h900_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
-"https://youtu.be/6ZfuNTqbHE8");
+                           "https://image.tmdb.org/t/p/w600_and_h90" +
+                           "0_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+                           "https://youtu.be/6ZfuNTqbHE8")
 
 
-movie_data = [avengers,infinity_war,zombieland,avatar,mad_max];
-fresh_tomatoes.open_movies_page(movie_data);
+movie_data = [avengers, infinity_war, zombieland, avatar, mad_max]
+fresh_tomatoes.open_movies_page(movie_data)
--- media.py
+++ media.py
@@ -1,9 +1,15 @@
 import webbrowser
+
+
+# a class specify the movie properites that will show on movie trailer website
 class Movie():
-    def __init__(self,movie_title,poster_image,trailer_youtube_url):
+    # Moive class constructor.
+    # __init__ gets called whenever we try to create object of class
+    def __init__(self, movie_title, poster_image, trailer_youtube_url):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube_url
-    
+
+    # method to open youtube trailer in popup
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
